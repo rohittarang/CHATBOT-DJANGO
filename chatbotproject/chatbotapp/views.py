76e832ef6806f0d28fdda1d8e0c264dff13aacff
rohittarang <rohittarang@gmail.com>
2023-06-17 22:22:10 +0530

    changes in view code

--- chatbotproject/chatbotapp/views.py
+++ chatbotproject/chatbotapp/views.py
@@ -1,53 +1,3 @@
-# ------------------------------- PART-1 ------------------------------- #
-
-# from django.shortcuts import render
-# from django.http import HttpResponse
-
-# #/------------------ chatbot-libraries ------------------/#
-
-# from chatterbot import ChatBot
-# from chatterbot.trainers import ListTrainer
-
-# from chatterbot import utils
-# from chatterbot.storage import SQLStorageAdapter
-
-# storage_adapter = {
-#     'import_path': 'chatterbot.storage.SQLStorageAdapter',
-#     'database_uri': 'sqlite:///D:/CHATBOT-DJANGO/chatbotproject/db.sqlite3',
-#     'read_only': False
-# }
-# bot = ChatBot('chatbot')
-# # bot = ChatBot('chatbot', storage_adapter=storage_adapter)
-# # bot = ChatBot('chatbot', read_only=False, logic_adapters=['chatterbot.logic.BestMatch'])
-# # bot = ChatBot('chatbot', read_only=False)
-
-# list_to_train = [
-#     "Hi", #question
-#     "Hi, there", #answer
-#     "What's Your Name", #question
-#     "I'm just a Chatbot", #answer
-#     "What is your favourite game ?", #question
-#     "My favourite game is Cricket", #answer
-# ]
-
-# list_trainer = ListTrainer(bot)
-# list_trainer.train(list_to_train)
-
-# #/------------------ chatbot-libraries ------------------/#
-
-# # Create your views here.
-# def index(request):
-#     return render(request,'chatbotapp/index.html')
-
-# def specific(request):
-#     return HttpResponse("This is specific url")
-
-# def getResponse(request):
-#     userMessage = request.GET.get('userMessage')
-#     return HttpResponse(userMessage)
-
-# ------------------------------- PART-2 ------------------------------- #
-
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -72,14 +22,8 @@ list_to_train = [
     "I'm just a Chatbot", #answer
     "What is your favourite game ?", #question
     "My favourite game is Cricket", #answer
-    "Is Vijeta Pagal ?",
-    "Yes, Vijeta is Pagal ... ",
-    "And What About Prilantee ?",
-    "Prilantee is Badiii Pagallll...", 
-    "Who is Archana ?",
-    "Archana is a beautiful girl with beautiful Mind",
-    "Any Other noticable things in Archana ?",
-    "Bas Kar Pagle. Isse jyada juth nai bol sakta...... !!! "
+    "What is your favourite food ? ", #question
+    "My favourite food is Internet", #answer
 ]
 
 # list_trainer = ListTrainer(bot)
